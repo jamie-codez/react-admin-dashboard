3f31a5092d7055c3ba4071284c72b57399d423ea
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-27 15:54:25 +0300

    Color palette working well 😎

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
+import { LightModeOutlined, NotificationsOutlined, PersonOutline, Search, SettingsOutlined } from "@mui/icons-material";
 
 const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
   return (
-    <div>Topbar</div>
-  )
-}
+    <Box>
+      {/* Search bar */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
+      </Box>
+      {/* Icons section */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutline />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
-export default Topbar
+export default Topbar;
